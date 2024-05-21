8d9e00c3f51eb2fad4666c6897cef2727e904f43
Eldan Kitai <eldankit@Eldans-MacBook-Pro.local>
2024-05-21 15:48:51 +0300

    made s3 bucket private added boto3 to access

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import boto3
+import os
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 db = SQLAlchemy(app)
@@ -11,20 +14,37 @@ class User(db.Model):
     email = db.Column(db.String(35), unique=False, nullable=False)
 
 
+s3_client = boto3.client('s3')
+S3_BUCKET = 'eldan-arsenal-buk1'
+S3_KEY = 'Arsenal_FC.png'
+
+def generate_presigned_url(bucket_name, object_key, expiration=3600):
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name, 'Key': object_key},
+                                                    ExpiresIn=expiration)
+    except Exception as e:
+        print(e)
+        return None
+    return response
+
 @app.route("/")
 def hello():
     user_list = User.query.all()
-    return render_template("index.html", user_list=user_list)
+    logo_url = generate_presigned_url(S3_BUCKET, S3_KEY)
+    return render_template("index.html", user_list=user_list, logo_url=logo_url)
 
 @app.route("/gunner/<int:user_id>")
 def Gunner_details(user_id):
     user = User.query.get(user_id)
-    return render_template("gunner_info.html", user=user)
+    logo_url = generate_presigned_url(S3_BUCKET, S3_KEY)
+    return render_template("gunner_info.html", user=user, logo_url=logo_url)
 
 @app.route("/hello")
 def Gunner_hello():
     username = request.args.get("username")
-    return render_template("gunner_hello.html", username=username)
+    logo_url = generate_presigned_url(S3_BUCKET, S3_KEY)
+    return render_template("gunner_hello.html", username=username, logo_url=logo_url)
 
 @app.route("/add_gunner", methods=["POST", "GET"])
 def AddGunner():
@@ -36,7 +56,8 @@ def AddGunner():
         db.session.commit()
         return redirect(url_for("Gunner_hello", username=username))
     else:
-        return render_template("add_gunner.html")
+        logo_url = generate_presigned_url(S3_BUCKET, S3_KEY)
+        return render_template("add_gunner.html", logo_url=logo_url)
 
     
 
